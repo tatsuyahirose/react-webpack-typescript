509486ee168beb22aa5a37ff67bad932894735f3
tatsuya_hirose <hirose_tatsuya_gn@cyberagent.co.jp>
2020-02-19 11:46:13 +0900

    wip

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,14 +1,19 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+
+import { createStore } from 'redux'
 import { Provider } from 'react-redux';
-import store from './store'
+
 import { AmebaPop } from './page/AmebaPop';
 import "../style/style.css";
 
-const rootElement = document.getElementById('root');
+import reducer from '../reducers/reducer';
+
+const store = createStore(reducer);
+
 ReactDOM.render(
   <Provider store={store}>
     <AmebaPop />
   </Provider>,
-  rootElement
+  document.getElementById('root')
 )
